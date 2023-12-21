61842630ab2db2c078ef9e30f284f5b490470f42
“Anatoli <humanities5050@gmail.com>
2023-12-21 21:33:15 +0300

    feat: solve transform array problem

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!Array.isArray(arr)) {
+		throw new Error("\'arr\' parameter must be an instance of the Array!");
+	}
+	let result = [];
+	let arrClone = JSON.parse(JSON.stringify(arr));
+	if (arrClone[0] === '--discard-prev' || arrClone[0] === '--double-prev') {
+		arrClone.splice(0, 1);
+	} else if (arrClone[arrClone.length - 1] === '--double-next' || arrClone[arrClone.length - 1] === '--discard-next') {
+		arrClone.splice(-1, 1);
+	} 
+	for (let i = 0; i < arrClone.length; i += 1) {
+		if (arrClone[i] === '--double-next') {
+			result.push(arrClone[i + 1])
+		} else if (arrClone[i] === '--discard-next') {
+			i += 1;
+		} else if (arrClone[i] === '--double-prev') {
+			result.push(result[result.length - 1])
+		} else if (arrClone[i] === '--discard-prev') {
+			result.pop();
+		} else {
+			result.push(arrClone[i]);
+		}
+	}
+	return result;
 }
 
 module.exports = {
-  transform
+	transform
 };
